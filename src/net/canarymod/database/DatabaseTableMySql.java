2102b59aa36b22a290c892d2e28c86cc0ebf1d0b
Devenias <devenias@gmx.de>
2012-06-15 01:33:56 +0200

    - class with methods for mysql table operations - please finish method addRow() - please review this class

--- src/net/canarymod/database/DatabaseTableMySql.java
+++ src/net/canarymod/database/DatabaseTableMySql.java
@@ -21,6 +21,10 @@ public class DatabaseTableMySql implements DatabaseTable {
 
 	@Override
 	public void setName(String name) {
+		if (name == null || name.isEmpty()) {
+			return;
+		}
+		
 		try {
 			PreparedStatement ps = DatabaseMySql.getStatement("ALERT TABLE ? RENAME ?");
 			ps.setString(0, _tableName);
@@ -51,6 +55,10 @@ public class DatabaseTableMySql implements DatabaseTable {
 
 	@Override
 	public void setDescription(String description) {
+		if (description == null) {
+			description = "";
+		}
+		
 		try {
 			PreparedStatement ps = DatabaseMySql.getStatement("ALTER TABLE ? COMMENT = ?");
 			ps.setString(0, _tableName);
@@ -80,6 +88,10 @@ public class DatabaseTableMySql implements DatabaseTable {
 
 	@Override
 	public DatabaseRow getRow(int row) {
+		if (row <= 0) {
+			return null;
+		}
+		
 		try {
 			PreparedStatement ps = DatabaseMySql.getStatement("SELECT * FROM ? LIMIT ?,1");
 			ps.setString(0, _tableName);
@@ -103,74 +115,319 @@ public class DatabaseTableMySql implements DatabaseTable {
 
 	@Override
 	public DatabaseRow[] getAllRows() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			PreparedStatement ps = DatabaseMySql.getStatement("SELECT * FROM ?");
+			ps.setString(0, _tableName);
+			
+			ResultSet rs = ps.executeQuery();
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int numColumns = rsmd.getColumnCount();
+			
+			// count Rows
+			rs.last();
+			int numRows = rs.getRow();
+			
+			rs.beforeFirst();
+			DatabaseRow[] result = new DatabaseRow[numRows];
+			HashMap<String, Object> columValues;
+			while (rs.next()) {
+				columValues = new HashMap<String, Object>();
+				for (int i = 0; i < numColumns; ++i)	{
+					columValues.put(rsmd.getColumnName(i).toUpperCase(), rs.getObject(i));
+				}
+				result[rs.getRow()-1] = new DatabaseRowMySql(columValues);
+			}
+			
+			return result;
+		} catch (SQLException e) {
+			// TODO Logging
+			return null;
+		}
 	}
 
 	@Override
 	public DatabaseRow[] getFilteredRows(String column, String value) {
-		// TODO Auto-generated method stub
-		return null;
+		if (column == null || column.isEmpty() || value == null || value.isEmpty()) {
+			return null;
+		}
+		
+		try {
+			PreparedStatement ps = DatabaseMySql.getStatement("SELECT * FROM ? WHERE ? = ?");
+			ps.setString(0, _tableName);
+			ps.setString(1, column);
+			
+			try {
+				ps.setInt(2, Integer.parseInt(value));
+			}
+			catch (Exception ex) {
+				try {
+					ps.setDouble(2, Double.parseDouble(value));
+				}
+				catch (Exception iex) {
+					ps.setString(2, value);
+				}
+			}
+			
+			ResultSet rs = ps.executeQuery();
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int numColumns = rsmd.getColumnCount();
+			
+			// count Rows
+			rs.last();
+			int numRows = rs.getRow();
+			
+			rs.beforeFirst();
+			DatabaseRow[] result = new DatabaseRow[numRows];
+			HashMap<String, Object> columValues;
+			while (rs.next()) {
+				columValues = new HashMap<String, Object>();
+				for (int i = 0; i < numColumns; ++i)	{
+					columValues.put(rsmd.getColumnName(i).toUpperCase(), rs.getObject(i));
+				}
+				result[rs.getRow()-1] = new DatabaseRowMySql(columValues);
+			}
+			
+			return result;
+		} catch (SQLException e) {
+			// TODO Logging
+			return null;
+		}
 	}
 
 	@Override
 	public boolean rowExists(String column, String value) {
-		// TODO Auto-generated method stub
-		return false;
+		if (column == null || column.isEmpty() || value == null || value.isEmpty()) {
+			return false;
+		}
+		
+		try {
+			PreparedStatement ps = DatabaseMySql.getStatement("SELECT * FROM ? WHERE ? = ? LIMIT 1");
+			ps.setString(0, _tableName);
+			ps.setString(1, column);
+			
+			try {
+				ps.setInt(2, Integer.parseInt(value));
+			}
+			catch (Exception ex) {
+				try {
+					ps.setDouble(2, Double.parseDouble(value));
+				}
+				catch (Exception iex) {
+					ps.setString(2, value);
+				}
+			}
+			
+			ResultSet rs = ps.executeQuery();
+			return rs.first();
+		}
+		catch (SQLException ex) {
+			// TODO Logging
+			return false;
+		}
 	}
 
 	@Override
 	public DatabaseRow addRow() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			// what should be inserted in the database????
+			PreparedStatement ps = DatabaseMySql.getStatement("INSERT INTO ? VALUES (LOL?, WTF?)");
+			ps.setString(0, _tableName);
+			
+			ResultSet rs = ps.executeQuery();
+			rs.first();
+			
+			ResultSetMetaData rsmd = rs.getMetaData();
+			HashMap<String, Object> columValues = new HashMap<String, Object>();
+						
+			for (int i = 0; i < rsmd.getColumnCount(); ++i)
+			{
+				columValues.put(rsmd.getColumnName(i).toUpperCase(), rs.getObject(i));
+			}
+			
+			return new DatabaseRowMySql(columValues);
+		}
+		catch (SQLException ex) {
+			// TODO Logging
+			return null;
+		}
 	}
 
 	@Override
 	public void removeRow(DatabaseRow row) {
-		// TODO Auto-generated method stub
+		try {
 
+			StringBuilder whereQuery = new StringBuilder();
+			// for numColumns in row build where query
+			// ? = ? AND ? = ? ...
+			
+			PreparedStatement ps = DatabaseMySql.getStatement("DELETE FROM ? WHERE " + whereQuery.toString());
+			ps.setString(0, _tableName);
+			
+			// for numColumns in row set values
+			// ps.setString(x, col);
+			// ps.setLong/setDouble/setString(x+1, val)
+			
+			ps.executeQuery();
+		}
+		catch (SQLException ex) {
+			// TODO Logging
+		}
 	}
 
 	@Override
 	public void removeRow(int row) {
-		// TODO Auto-generated method stub
-
+		if (row <= 0) {
+			return;
+		}
+		
+		try {
+			// does the query works?
+			PreparedStatement ps = DatabaseMySql.getStatement("DELETE FROM ? WHERE (SELECT * FROM ? LIMIT ?,1)");
+			ps.setString(0, _tableName);
+			ps.setString(1, _tableName);
+			ps.setInt(2, row-1);
+			
+			ps.executeQuery();
+		}
+		catch (SQLException ex) {
+			// TODO Logging
+		}
 	}
 
 	@Override
 	public int getNumColumns() {
-		// TODO Auto-generated method stub
-		return 0;
+		try {
+			PreparedStatement ps = DatabaseMySql.getStatement("SELECT * FROM ? LIMIT 1");
+			ps.setString(0, _tableName);
+			
+			ResultSet rs = ps.executeQuery();
+			ResultSetMetaData rsmd = rs.getMetaData();
+			
+			return rsmd.getColumnCount();
+		}
+		catch (SQLException ex) {
+			// TODO Logging
+			return -1;
+		}
 	}
 
 	@Override
 	public String[] getAllColumns() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			PreparedStatement ps = DatabaseMySql.getStatement("SELECT * FROM ? LIMIT 1");
+			ps.setString(0, _tableName);
+			
+			ResultSet rs = ps.executeQuery();
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int numColumns = rsmd.getColumnCount();
+
+			String[] result = new String[numColumns];
+			for (int i = 0; i < numColumns; ++i)	{
+				result[i] = rsmd.getColumnName(i).toUpperCase();
+			}
+			
+			return result;
+		}
+		catch (SQLException ex) {
+			// TODO Logging
+			return new String[0];
+		}
 	}
 
 	@Override
 	public void appendColumn(String name, ColumnType type) {
-		// TODO Auto-generated method stub
-
+		try {
+			
+			// TODO check for existing column
+			
+			String strType = "";
+			
+			if (type == ColumnType.BOOLEAN)
+				strType = "TINYINT";
+			else if (type == ColumnType.INTEGER)
+				strType = "INTEGER";
+			else if (type == ColumnType.LONG)
+				strType = "BIGINT";
+			else if (type == ColumnType.FLOAT)
+				strType = "FLOAT";
+			else if (type == ColumnType.DOUBLE)
+				strType = "DOUBLE";
+			else if (type == ColumnType.CHARACTER)
+				strType = "CHAR";
+			else if (type == ColumnType.STRING)
+				strType = "VARCHAR(max)";
+			
+			PreparedStatement ps = DatabaseMySql.getStatement("ALTER TABLE ? ADD COLUMN ? " + strType + " NULL");
+			ps.setString(0, _tableName);
+			ps.setString(1, name);
+			
+			ps.executeQuery();
+		}
+		catch (SQLException ex) {
+			// TODO Logging
+		}
 	}
 
 	@Override
 	public void removeColumn(String name) {
-		// TODO Auto-generated method stub
-
+		try {
+			PreparedStatement ps = DatabaseMySql.getStatement("ALTER TABLE ? DROP COLUMN ?");
+			ps.setString(0, _tableName);
+			ps.setString(1, name);
+			
+			ps.executeQuery();
+		}
+		catch (SQLException ex) {
+			// TODO Logging
+		}
 	}
 
 	@Override
 	public void insertColumn(String name, ColumnType type, String after) {
-		// TODO Auto-generated method stub
-
+		try {
+			
+			// TODO check for existing column
+			// TODO check column "after" exists
+			
+			String strType = "";
+			
+			if (type == ColumnType.BOOLEAN)
+				strType = "TINYINT";
+			else if (type == ColumnType.INTEGER)
+				strType = "INTEGER";
+			else if (type == ColumnType.LONG)
+				strType = "BIGINT";
+			else if (type == ColumnType.FLOAT)
+				strType = "FLOAT";
+			else if (type == ColumnType.DOUBLE)
+				strType = "DOUBLE";
+			else if (type == ColumnType.CHARACTER)
+				strType = "CHAR";
+			else if (type == ColumnType.STRING)
+				strType = "VARCHAR(max)";
+			
+			PreparedStatement ps = DatabaseMySql.getStatement("ALTER TABLE ? ADD COLUMN ? " + strType + " NULL AFTER ?");
+			ps.setString(0, _tableName);
+			ps.setString(1, name);
+			ps.setString(2, after);
+			
+			ps.executeQuery();
+		}
+		catch (SQLException ex) {
+			// TODO Logging
+		}
 	}
 
 	@Override
 	public void truncateTable() {
-		// TODO Auto-generated method stub
-
+		try {
+			PreparedStatement ps = DatabaseMySql.getStatement("TRUNCATE TABLE ?");
+			ps.setString(0, _tableName);
+			
+			ps.executeQuery();
+		}
+		catch (SQLException ex) {
+			// TODO Logging
+		}
 	}
-
 }
