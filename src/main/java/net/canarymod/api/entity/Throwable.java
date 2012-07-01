dcfd6bba47789dd1adc4598d89f57f984348915c
Logan Kenwright <Loganvz@gmail.com>
2012-07-01 13:09:21 +1200

    Added Throwable interface.

---
+++ src/main/java/net/canarymod/api/entity/Throwable.java
@@ -0,0 +1,10 @@
+package net.canarymod.api.entity;
+
+public interface Throwable extends Entity{
+    public void getType();
+    /**
+     * Returns the type of this throwable.
+     * 
+     */
+
+}
