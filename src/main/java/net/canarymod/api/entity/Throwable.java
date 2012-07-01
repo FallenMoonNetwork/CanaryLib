78125eb3631e0a7e63b2e6192737e10bceee8d32
Logan Kenwright <Loganvz@gmail.com>
2012-07-01 15:00:03 +1200

    Added ThrowableType Enum.

--- src/main/java/net/canarymod/api/entity/Throwable.java
+++ src/main/java/net/canarymod/api/entity/Throwable.java
@@ -1,10 +1,18 @@
 package net.canarymod.api.entity;
 
 public interface Throwable extends Entity{
+    /**
+     * @author Logan Kenwright
+     *
+     */
     public void getType();
     /**
      * Returns the type of this throwable.
      * 
+     */
+    public void getSource(net.canarymod.api.entity.EntityLiving source);
+    /**
+     * Gets the source of the throwable.
      */
 
 }
---
+++ src/main/java/net/canarymod/api/entity/ThrowableType.java
@@ -0,0 +1,9 @@
+package net.canarymod.api.entity;
+
+public enum ThrowableType {
+    SNOWBALL,
+    EGG,
+    ENDERPEARL,
+    
+
+}
