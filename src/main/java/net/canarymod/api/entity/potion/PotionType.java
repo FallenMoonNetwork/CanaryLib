bda58e0d10af0d4b9ab9fc4e20fb4c6f811b02ee
Brian McCarthy <brianmccarthy@windowslive.com>
2012-07-16 19:38:24 +1200

    Change in PotionType

--- src/main/java/net/canarymod/api/entity/potion/PotionType.java
+++ src/main/java/net/canarymod/api/entity/potion/PotionType.java
@@ -6,25 +6,25 @@ package net.canarymod.api.entity.potion;
  *
  */
 public enum PotionType {
-    MoveSpeed,
-    MoveSlowdown,
-    DigSpeed,
-    DigSlowdown,
-    DamageBoost,
-    Heal,
-    Harm,
-    Jump,
-    Confusion,
-    Regeneration,
-    Resistance,
-    FireResistance,
-    WaterBreathing,
-    Invisibility,
-    Blindness,
-    NightVision,
-    Hunger,
-    Weakness,
-    Poison;
+    MOVE_SPEED,
+    MOVE_SLOWDOWN,
+    DIG_SPEED,
+    DIG_SLOWDOWN,
+    DAMAGE_BOOST,
+    HEAL,
+    HARM,
+    JUMP,
+    CONFUSION,
+    REGENERATION,
+    RESISTANCE,
+    FIRE_RESISTANCE,
+    WATER_BREATHING,
+    INVISIBILITY,
+    BLINDNESS,
+    NIGHTVISION,
+    HUNGER,
+    WEAKNESS,
+    POISON;
     
     /**
      * Get a {@link PotionType} from a name
@@ -32,6 +32,11 @@ public enum PotionType {
      * @return PotionTYpe or null if not valid name
      */
     public static PotionType fromName(String name) {
+        if (name.indexOf(".") != -1) {
+            name = name.substring(name.indexOf(".") + 1);
+        }
+        name = name.toUpperCase();
+        
         for (PotionType type : values()) {
             if (type.name() == name) {
                 return type;
