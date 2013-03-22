aa835dcbdb75b4dacb8b40bb5836013423abb910
damagefilter <chris@playblack.net>
2013-03-22 19:37:04 +0100

    Refactored BlockType
    It's now a mix of hashmap <name,blocktype> and a
    collection of static fields for vanilla block types

---
+++ src/main/java/net/canarymod/api/world/blocks/CustomBlockTypeException.java
@@ -0,0 +1,10 @@
+package net.canarymod.api.world.blocks;
+
+public class CustomBlockTypeException extends RuntimeException {
+
+    private static final long serialVersionUID = -1135339551333207850L;
+
+    public CustomBlockTypeException(String str) {
+        super(str);
+    }
+}
