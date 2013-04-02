86eaebc157ed66d8657d0d181531c58eb062dfb1
darkdiplomat <darkdiplomat@visualillusionsent.net>
2013-04-01 23:56:07 -0500

    Stat stuff

---
+++ src/main/java/net/canarymod/api/statistics/Stat.java
@@ -0,0 +1,34 @@
+package net.canarymod.api.statistics;
+
+/**
+ * Stat wrapper
+ * 
+ * @author Jason (darkdiplomat)
+ */
+public interface Stat {
+
+    /**
+     * Get the id of this stat.
+     * May not be very useful apart from comparing equality of stats with different names.
+     * E.G. if Minecraft changes the name
+     * 
+     * @return the Stat ID
+     */
+    public int getId();
+
+    /**
+     * Gets the name of the stat.
+     * NOTE: This has gone through Minecraft's string translate to get a readable form/
+     * 
+     * @return the name of the Stat
+     */
+    public String getName();
+
+    /**
+     * Return is this stat is independent (i.e., lacking prerequisites for being updated)
+     * 
+     * @return {@code true} if independent; {@code false} if not
+     */
+    public boolean isIndependent();
+
+}
---
+++ src/main/java/net/canarymod/hook/player/StatGainedHook.java
@@ -0,0 +1,48 @@
+package net.canarymod.hook.player;
+
+import net.canarymod.api.entity.living.humanoid.Player;
+import net.canarymod.api.statistics.Stat;
+import net.canarymod.hook.CancelableHook;
+
+/**
+ * StatGainedHook<br>
+ * Called when a {@link Player} gains a Stat
+ * 
+ * @author Jason (darkdiplomat)
+ */
+public final class StatGainedHook extends CancelableHook {
+    private Player player;
+    private Stat stat;
+
+    /**
+     * Constructs a new StatGainedHook
+     * 
+     * @param player
+     *            the {@link Player} gaining a stat
+     * @param stat
+     *            the {@link Stat} being gained
+     */
+    public StatGainedHook(Player player, Stat stat) {
+        this.player = player;
+        this.stat = stat;
+    }
+
+    /**
+     * Gets the {@link Player} gaining a {@link Stat}
+     * 
+     * @return the {@link Player}
+     */
+    public Player getPlayer() {
+        return player;
+    }
+
+    /**
+     * Gets the {@link Stat} being gained
+     * 
+     * @return the {@link Stat}
+     */
+    public Stat getStat() {
+        return stat;
+    }
+
+}
