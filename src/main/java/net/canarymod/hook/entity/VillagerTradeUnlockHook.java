109e2a28d75fc8256006da5a87e94f361182470a
darkdiplomat <darkdiplomat@visualillusionsent.net>
2013-04-02 01:33:05 -0500

    VillagerTradeUnlock added

---
+++ src/main/java/net/canarymod/hook/entity/VillagerTradeUnlockHook.java
@@ -0,0 +1,48 @@
+package net.canarymod.hook.entity;
+
+import net.canarymod.api.VillagerTrade;
+import net.canarymod.api.entity.living.humanoid.Villager;
+import net.canarymod.hook.CancelableHook;
+
+/**
+ * VillagerTradeUnlock hook<br>
+ * Called when a Trade is unlocked
+ * 
+ * @author Jason (darkdiplomat)
+ */
+public final class VillagerTradeUnlockHook extends CancelableHook {
+    private Villager villager;
+    private VillagerTrade trade;
+
+    /**
+     * Constructs a new VillagerTradeUnlockHook
+     * 
+     * @param villager
+     *            the {@link Villager} merchant
+     * @param trade
+     *            the {@link VillagerTrade} unlocked
+     */
+    public VillagerTradeUnlockHook(Villager villager, VillagerTrade trade) {
+        this.villager = villager;
+        this.trade = trade;
+    }
+
+    /**
+     * Gets the {@link Villager} merchant
+     * 
+     * @return the {@link Villager}
+     */
+    public Villager getVillager() {
+        return villager;
+    }
+
+    /**
+     * Gets the {@link VillagerTrade} unlocked
+     * 
+     * @return the {@link VillagerTrade} unlocked
+     */
+    public VillagerTrade getTrade() {
+        return trade;
+    }
+
+}
