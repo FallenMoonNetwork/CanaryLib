1fd08a8c0100c4aa98f6d096ff2d40621799593c
darkdiplomat <darkdiplomat@visualillusionsent.net>
2013-04-02 20:39:19 -0500

    PlayerLeftClick

---
+++ src/main/java/net/canarymod/hook/player/PlayerLeftClickHook.java
@@ -0,0 +1,28 @@
+package net.canarymod.hook.player;
+
+import net.canarymod.api.entity.living.humanoid.Player;
+import net.canarymod.hook.Hook;
+
+/**
+ * Player Left Click Hook<br>
+ * Called when a {@link Player} swings their arm with a Left Click
+ * 
+ * @author Jason (darkdiplomat)
+ */
+public class PlayerLeftClickHook extends Hook {
+    private Player player;
+
+    public PlayerLeftClickHook(Player player) {
+        this.player = player;
+    }
+
+    /**
+     * Gets the {@link Player} clicking
+     * 
+     * @return the {@link Player}
+     */
+    public Player getPlayer() {
+        return player;
+    }
+
+}
