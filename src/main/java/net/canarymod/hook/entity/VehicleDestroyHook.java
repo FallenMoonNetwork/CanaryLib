a73666a360da1f371481722fd0a8ecfbf0db75b1
darkdiplomat <darkdiplomat@visualillusionsent.net>
2013-04-03 08:28:50 -0500

    VehicleDestroy and VehicleCollision changes

---
+++ src/main/java/net/canarymod/hook/entity/VehicleDestroyHook.java
@@ -0,0 +1,32 @@
+package net.canarymod.hook.entity;
+
+import net.canarymod.api.entity.vehicle.Vehicle;
+import net.canarymod.hook.Hook;
+
+/**
+ * VehicleDestoryHook<br>
+ * Called when a Minecart or Boat is destroyed
+ * 
+ * @author Jason (darkdiplomat)
+ */
+public class VehicleDestroyHook extends Hook {
+    private Vehicle vehicle;
+
+    public VehicleDestroyHook(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    /**
+     * The {@link Vehicle} being destoryed
+     * 
+     * @return the {@link Vehicle}
+     */
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    @Override
+    public final String toString() {
+        return String.format("%s[Vehicle=%s]", getName(), vehicle);
+    }
+}
