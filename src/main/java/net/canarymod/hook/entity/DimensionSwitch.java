4862c232c4b5220d378940d87b7f3e393e97e1cd
Somners <somers.ans@gmail.com>
2013-04-06 15:14:37 -0400

    DimensionSwitch Added.

---
+++ src/main/java/net/canarymod/hook/entity/DimensionSwitch.java
@@ -0,0 +1,45 @@
+package net.canarymod.hook.entity;
+
+import net.canarymod.api.entity.Entity;
+import net.canarymod.api.world.position.Location;
+import net.canarymod.hook.CancelableHook;
+
+/**
+ *
+ * @author Somners
+ */
+public class DimensionSwitch extends CancelableHook {
+
+    private Entity entity;
+    private Location to, from;
+
+    public DimensionSwitch(Entity entity, Location to, Location from) {
+        this.entity = entity;
+        this.to = to;
+        this.from = from;
+    }
+
+    /**
+     * Get the entity involved here.
+     * @return The entity.
+     */
+    public Entity getEntity() {
+        return this.entity;
+    }
+
+    /**
+     * Get the entities current location.
+     * @return Current Location
+     */
+    public Location getLocationTo() {
+        return this.to;
+    }
+
+    /**
+     * Get the location the entity is warping to.
+     * @return Location to.
+     */
+    public Location getLocationFrom() {
+        return this.from;
+    }
+}
