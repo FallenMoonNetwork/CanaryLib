43c1dfc6d8e2dc8a0f4f7d0af5d36f6ff8cd7add
darkdiplomat <darkdiplomat@visualillusionsent.net>
2013-04-07 10:08:17 -0500

    Furnace update

--- src/main/java/net/canarymod/api/world/blocks/Furnace.java
+++ src/main/java/net/canarymod/api/world/blocks/Furnace.java
@@ -3,5 +3,41 @@ package net.canarymod.api.world.blocks;
 
 import net.canarymod.api.inventory.Inventory;
 
+/**
+ * Furnace wrapper
+ * 
+ * @author Jason (darkdiplomat)
+ */
+public interface Furnace extends ComplexBlock, Inventory {
+    /**
+     * Returns the number of ticks the current fuel item has to go.
+     * 
+     * @return burn time ticks
+     */
+    public short getBurnTime();
 
-public interface Furnace extends ComplexBlock, Inventory {}
+    /**
+     * Sets the number of ticks the current fuel item has to go.
+     * 
+     * @param time
+     *            ticks of burning left
+     */
+    public void setBurnTime(short time);
+
+    /**
+     * Returns the number of ticks the item to smelt has smolten.
+     * An item is ready on 200 ticks.
+     * 
+     * @return cook time ticks
+     */
+    public short getCookTime();
+
+    /**
+     * Sets the number of ticks the item to smelt has smolten.
+     * An item is ready on 200 ticks.
+     * 
+     * @param time
+     *            ticks of cooking
+     */
+    public void setCookTime(short time);
+}
