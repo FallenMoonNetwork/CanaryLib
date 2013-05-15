b9944ce8f6a59e726a106019a14274544cd51969
Larry1123 <1123larry@live.com>
2013-05-14 22:05:04 -0400

    GUI api interface

---
+++ src/main/java/net/canarymod/api/gui/GUIControl.java
@@ -0,0 +1,23 @@
+package net.canarymod.api.gui;
+
+public interface GUIControl {
+
+    /**
+     * Used when the GUI is to be replaced.
+     * Should be used to Close your GUI with out stopping the server
+     */
+    public void closeWindow();
+
+    /**
+     * Called to start the GUI
+     * @return
+     */
+    public void start();
+
+    /**
+     * The name of the GUI
+     * @return
+     */
+    public String getName();
+
+}
