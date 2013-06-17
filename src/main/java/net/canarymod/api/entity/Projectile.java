fb32840229c48053cc198f426fe289aeb27a0383
Larry1123 <1123larry@live.com>
2013-06-17 03:04:51 -0400

    KIlled Tabs!

--- src/main/java/net/canarymod/api/entity/Projectile.java
+++ src/main/java/net/canarymod/api/entity/Projectile.java
@@ -1,15 +1,16 @@
 package net.canarymod.api.entity;
 
-public interface Projectile {
+public interface Projectile{
 
-	/**
-	 * Sets the Motion of a Projectile and Rotation
-	 * @param motionX
-	 * @param motionY
-	 * @param motionZ
-	 * @param rotationYaw
-	 * @param rotationPitch
-	 */
-	public void setProjectileHeading(double motionX, double motionY, double motionZ, float rotationYaw, float rotationPitch);
+    /**
+     * Sets the Motion of a Projectile and Rotation
+     * 
+     * @param motionX
+     * @param motionY
+     * @param motionZ
+     * @param rotationYaw
+     * @param rotationPitch
+     */
+    public void setProjectileHeading(double motionX, double motionY, double motionZ, float rotationYaw, float rotationPitch);
 
 }
